d24a32828c11cb3675f4d6fdec0548834853e912
Ditunn <161791337+Ditunn@users.noreply.github.com>
2025-07-08 20:44:45 +0530

    Update largest_num.js

--- Day_1/largest_num.js
+++ Day_1/largest_num.js
@@ -1,5 +1,5 @@
 function largest_Number() {
-  let array = [10, 30, 60, 120, 90, 55];
+  let array = [10, 20, 30, 400, 60, 100];
   let largest = array[1];
   for (let i = 0; i < array.length; i++) {
     if (array[i] > largest) {
