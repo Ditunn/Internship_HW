aed94d5ddfe28b85961542eb5a4f413f30cc810c
Ditunn <161791337+Ditunn@users.noreply.github.com>
2025-07-08 20:42:15 +0530

    Update even_number.js

--- Day_1/even_number.js
+++ Day_1/even_number.js
@@ -1,5 +1,5 @@
 function even_Number() {
-  let num = [8, 9, 11, 16, 24, 43, 57, 65];
+  let num = [4, 6, 7, 15, 24, 44, 52, 55];
   for (let i = 0; i < num.length; i++) {
     if (num[i] % 2 == 0) {
       console.log(num[i]);
